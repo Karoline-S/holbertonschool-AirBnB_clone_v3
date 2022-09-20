8910a3a7192961a1ebe23901c8cefe81d70f0448
Karoline-S <karoline.amy@gmail.com>
2022-09-20 06:52:18 +0000

    practice - Flask api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,16 @@
 from flask import Flask
 from models import storage
 from os import getenv
-import api.v1.views from app_views
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-@app.teardown.appcontext
+@app.teardown_appcontext
 def close_session(exception=None):
     """ends the sql alchemy session"""
     storage.close()
 
-if getenv(HBNB_API_HOST):
-    HOST = getenv(HBNB_API_HOST)
-else:
-    HOST = "0.0.0.0"
-
-if getenv(HBNB_API_PORT):
-    PORT = HBNB_API_PORT
-else:
-    PORT = 5000
 
 if __name__ == '__main__':
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=getenv("HBNB_API_PORT", 5000), threaded=True)
