b6d215507a34a017a682c21efa058c30065d8cae
Jql11 <jaqueling@126.com>
2022-09-23 11:50:53 +0000

    remove assertIs test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -121,9 +121,7 @@ class TestFileStorage(unittest.TestCase):
         '''
         new_o = State(name="Vic")
         obj = models.storage.get("State", "1234")
-        obj_t = models.storage.get("State", new_o.id)
         self.assertIsNone(obj)
-        self.assertIs(new_o, obj_t)
 
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_fs_storage_count(self):
